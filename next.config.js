36ebf9b7239b08ad890385bccba5dacc41985923
ShreyanshBB <bhagwatsb@rknec.edu>
2025-08-28 22:11:35 +0530

    using github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
